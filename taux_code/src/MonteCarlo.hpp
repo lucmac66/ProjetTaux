c6308a69727f397334a556e15228fdc8ff92f815
Ayoub Miguil <miguila@ensipc131.ensimag.fr>
2023-02-07 11:01:29 +0100

    header modification MC

--- taux_code/src/MonteCarlo.hpp
+++ taux_code/src/MonteCarlo.hpp
@@ -13,42 +13,5 @@ class MonteCarlo
     double fdStep_;          /*! pas de différence finie */
     long nbSamples_;         /*! nombre de tirages Monte Carlo */
 
-    /**
-     * Calcule le prix de l'option à la date 0
-     *
-     * @param[out] prix valeur de l'estimateur Monte Carlo
-     * @param[out] ic écart type de l'estimateur
-     */
-    void price(double& prix, double& std_dev);
-
-    /**
-     * Calcule le prix de l'option à la date t
-     *
-     * @param[in]  past contient la trajectoire du sous-jacent
-     * jusqu'à l'instant t
-     * @param[in] t date à laquelle le calcul est fait
-     * @param[out] prix contient le prix
-     * @param[out] std_dev contient l'écart type de l'estimateur
-     */
-    void price(const PnlMat* past, double t, double& prix, double& std_dev);
-
-    /**
-     * Calcule le delta de l'option à la date t
-     *
-     * @param[in] past contient la trajectoire du sous-jacent
-     * jusqu'à l'instant t
-     * @param[in] t date à laquelle le calcul est fait
-     * @param[out] delta contient le vecteur de delta
-     * @param[out] std_dev contient l'écart type de l'estimateur
-     */
-    void delta(const PnlMat* past, double t, PnlVect* delta, PnlVect* std_dev);
-
-    /**
-     * Calcule le delta de l'option à la date 0
-     *
-     * @param[in] t date à laquelle le calcul est fait
-     * @param[out] delta contient le vecteur de delta
-     * @param[out] std_dev contient l'écart type de l'estimateur
-     */
-    void delta(PnlVect* delta, PnlVect* std_dev);
+    void priceAndDeltas(const PnlMat* past, double t, double& prix, double& std_dev, PnlVect *deltas, PnlVect *stdDeltas);
 };
